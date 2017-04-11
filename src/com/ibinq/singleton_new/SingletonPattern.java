08b863e780dc681c794f8a4db6a3693d5240c668
zhoubing <zhoubingl_l@163.com>
2017-04-11 10:38:52 +0800

    新单例模式

---
+++ src/com/ibinq/singleton_new/SingletonPattern.java
@@ -0,0 +1,15 @@
+package com.ibinq.singleton_new;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ * 通用单例模式
+ */
+public class SingletonPattern {
+    private static final SingletonPattern singletonPattern= new SingletonPattern();
+    //限制住不能直接产生一个实例
+    private SingletonPattern(){
+    }
+    public synchronized static SingletonPattern getInstance(){
+        return singletonPattern;
+    }
+}
