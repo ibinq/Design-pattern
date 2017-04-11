23cd996b95c34a4c2ca1769572f012761592d100
zhoubing <zhoubingl_l@163.com>
2017-04-11 10:26:24 +0800

    策略模式

---
+++ src/com/ibinq/proxy/JiaShi.java
@@ -0,0 +1,16 @@
+package com.ibinq.proxy;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class JiaShi implements KindWomen {
+    @Override
+    public void makeEyesWithMan() {
+        System.out.println("贾氏抛媚眼");
+    }
+
+    @Override
+    public void happyWithMan() {
+        System.out.println("贾氏正在Happy中......");
+    }
+}
---
+++ src/com/ibinq/proxy/PanJinLian.java
@@ -0,0 +1,16 @@
+package com.ibinq.proxy;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class PanJinLian implements KindWomen {
+    @Override
+    public void makeEyesWithMan() {
+        System.out.println("潘金莲抛媚眼");
+    }
+
+    @Override
+    public void happyWithMan() {
+        System.out.println("潘金莲在和男人做那个.....");
+    }
+}
