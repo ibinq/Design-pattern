16c8f5f2c29fcfba24c1633fe18ce4d481487b1b
zhoubing <zhoubingl_l@163.com>
2017-04-12 14:05:32 +0800

    add

---
+++ src/reasources/mysql.sql
@@ -0,0 +1,10 @@
+#IN#
+SELECT column_name(s)
+      FROM table_name
+      WHERE column_name IN (value1,value2,...)
+
+#BETWEEN #
+SELECT column_name(s)
+      FROM table_name
+      WHERE column_name
+      BETWEEN value1 AND value2
